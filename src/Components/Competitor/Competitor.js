a1054a4deff8f16865911656e95f8d53760251b1
Marko Puzovic <markopuzovic98@gmail.com>
2021-05-15 14:57:30 +0200

    competitor card width

--- src/Components/Competitor/Competitor.js
+++ src/Components/Competitor/Competitor.js
@@ -9,7 +9,7 @@ function Competitor({ addCompetitorHandler, show }) {
   const age = useRef();
   return (
     <div
-      className={`bg-light col-md-12 col-sm-12 col-12 mt-4 m-0${
+      className={`bg-light col-md-12 col-sm-12 col-12 mt-4 m-0 ${
         show ? "d-block" : "d-none"
       }`}
     >
